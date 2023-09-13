db5daf83c9f1f997edf695f6f4d47c1ff9c70a5a
PoojaMali18 <141479524+PoojaMali18@users.noreply.github.com>
2023-09-14 02:42:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
 function decimalToBinary(num) {
-  //Write you code here
-  
-}
+  // Check if the input is not a positive integer
+  if (!Number.isInteger(num) || num < 0) {
+    return "Invalid input. Please provide a non-negative integer.";
+  }
 
+  // Handle the special case of 0
+  if (num === 0) {
+    return "0";
+  }
+
+  let binary = ""; // Initialize an empty string to store the binary representation
+
+  // Convert the decimal number to binary
+  while (num > 0) {
+    binary = (num % 2) + binary; // Get the remainder and prepend it to the binary string
+    num = Math.floor(num / 2); // Divide the number by 2
+  }
+
+  return binary;
+}
 window.decimalToBinary = decimalToBinary;
+
+
